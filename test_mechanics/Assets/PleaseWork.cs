4e68a87c6fc1dab9ba8806684010bab7b2ff9311
kirazu <nikolay.vstavsky@gmail.com>
2024-07-13 14:34:44 +0300

    pleasework

---
+++ test_mechanics/Assets/PleaseWork.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PleaseWork : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    public GameObject camera1;
+
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+    public bool change = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("change")){
+            change =! change;
+            
+        }
+            if(change){
+                camera1.SetActive(true);
+                horizontalMove = Input.GetAxisRaw("Horizontal")* runSpeed;
+                if (Input.GetButtonDown("Jump"))
+                {
+                    jump = true;
+                }
+        }
+    } 
+
+    public void OnLanding(){
+    }
+
+    void FixedUpdate(){
+        
+        controller.Move(horizontalMove* Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
